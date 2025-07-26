bfc1593d7a8567f47fbc50b4d9dab424afa0c639
Andrei Ilin <stariktenger@gmail.com>
2025-07-26 14:34:04 +0200

    collection changed

--- scripts/Item.gd
+++ scripts/Item.gd
@@ -5,6 +5,31 @@ class_name Item
 
 @onready var sprite := $Sprite2D
 
+enum ItemState {
+	GROUND, # lying on the ground, initial state
+	ROBOT, # being hold by a robot 
+	IN_FLIGHT, # in the air
+	CONSUMED # being consumed by the slug
+}
+
+var state: ItemState = ItemState.GROUND
+var current_owner: Node = null # robot or slug holding or about to hold the egg
+
+# Source and destination positions, need for throwing the egg
+var source_pos: Vector2 = Vector2(0,0)
+var destination_pos: Vector2 = Vector2(0,0)
+
+var flight_progress = 0 # 0 - start of flight, 1 - landing
+var flight_progress_speed = 0 # Calculated from distance when throwing
+
+# TODO: egg flight animation, it should be not straight line
+
+func collect_from_ground(owner: Node):
+	current_owner = owner
+	state = ItemState.ROBOT
+	print("func called")
+	
+
 func _ready():
 	add_to_group("items")
 	match item_name:
--- scripts/robots/Robot.gd
+++ scripts/robots/Robot.gd
@@ -1,33 +1,34 @@
 extends CharacterBody2D
 class_name CollectorRobot
 
+# Stats
 @export var collect_radius: float = 64.0
-@export var max_capacity: int = 10
-var storage: Dictionary = {}
-var stored_type: String = ""
+@export var toss_radius: float = 256.0
+@export var cooldown: float = 1 # time between catch and throw
+@export var max_velocity: float = 1 # TODO: value
+@export var tossing_speed: float = 100 # egg speed when tossing
+
+var holding_egg: Node = null # Reference to the egg
+var busy_waiting: bool = false # If true, waiting for egg to arrive, cannot accept egg proposals
 
 @export var collect_delay: float = 1.0  # задержка в секундах
 var collect_timer := 1.0
 
-func _physics_process(delta):
+func try_collection(delta):
 	collect_timer -= delta
 	if collect_timer > 0:
-		return
-	print("Stored: ", storage)
+		return 
+	if holding_egg != null:
+		return # Already holding an egg
 	for item in get_tree().get_nodes_in_group("items"):
 		if position.distance_to(item.position) <= collect_radius:
-			if stored_type == "":
-				stored_type = item.item_name
-			if item.item_name != stored_type:
-				continue
-			# Собираем
-
-			var current_amount = storage.get(stored_type, 0)
-			if current_amount >= max_capacity:
-				return  # перегруз — ничего не собираем
-
-			storage[stored_type] = storage.get(stored_type, 0) + 1
-			item.queue_free()
+			
+			holding_egg = item
+			item.collect_from_ground(self)
+			
 			collect_timer = collect_delay  # сбрасываем таймер
-			print("Собрал: ", stored_type, ", всего: ", storage[stored_type])
 			break
+
+func _physics_process(delta):
+	collect_timer -= delta
+	try_collection(delta)
