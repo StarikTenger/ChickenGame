43a084aa7a29e666f0b451df91960e6fa903154d
Andrei Ilin <stariktenger@gmail.com>
2025-07-27 19:51:21 +0200

    animated lake

--- scenes/map/lake.gd
+++ scenes/map/lake.gd
@@ -6,3 +6,20 @@ extends StaticBody2D
 func _ready():
 	poly2d.polygon = collision.polygon
 	poly2d.position = collision.position
+
+var water_textures = [
+	load("res://sprites/water0000.png"),
+	load("res://sprites/water0001.png"),
+	load("res://sprites/water0002.png")
+]
+
+var frame = 0
+var frame_delay = 40
+var frame_counter = 0
+
+func _process(delta):
+	frame_counter += 1
+	if frame_counter >= frame_delay:
+		frame_counter = 0
+		frame = (frame + 1) % water_textures.size()
+		poly2d.texture = water_textures[frame]
